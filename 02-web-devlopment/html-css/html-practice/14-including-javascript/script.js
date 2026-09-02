55f22a6b15e38fe00032e5e9cf626bf586115df1
Heet Popat <popatheet8@gmail.com>
2026-09-02 17:46:10 +0530

    Add JavaScript integration practice

---
+++ 02-web-devlopment/html-css/html-practice/14-including-javascript/script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("title");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  title.textContent = "Hello Javascript!";
+});
